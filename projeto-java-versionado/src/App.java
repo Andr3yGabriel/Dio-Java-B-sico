fc168de6310427014aa39c84698e76ebef36e323
Andrey Gonçalves <andreygoncalvesapple@gmail.com>
2024-03-12 19:08:19 -0300

    Commit no IFB

--- projeto-java-versionado/src/App.java
+++ projeto-java-versionado/src/App.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bom dia, galera");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome: ");
+
+        String nome = input.nextLine();
+
+        System.out.println("Boa noite, " + nome);
+
+        input.close();
         
     }
 }
